bd37ffca443fb49515eac9f1146869453f466044
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2016-04-02 15:06:04 -0700

    removed default env values

--- src/com/pcd/Main.java
+++ src/com/pcd/Main.java
@@ -4,11 +4,10 @@ import cpabe.Cpabe;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
         String PUB_FILE = System.getenv("PCD_PUB_FILE");
         String PRV_FILE = System.getenv("PCD_PRV_FILE");
         String enc_file = args[0];
-        String dec_file = "dec_" + enc_file;
+        String dec_file = enc_file + "dec" ;
         Cpabe bridge = new Cpabe();
         try {
             bridge.dec(PUB_FILE, PRV_FILE, enc_file, dec_file);
